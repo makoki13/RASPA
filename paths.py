22b5a6b9845e4050aaca5b9b36b7391b43538996
makoki13 <pablo.makoki@gmail.com>
2026-06-24 23:52:42 +0200

    error en paths

--- paths.py
+++ paths.py
@@ -7,3 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
 TEMPLATE_DIR = BASE_DIR / "templates"
 GPX_TEMP_DIR = BASE_DIR / "gpx_temp"
+
+# LA MAGIA: Forzar la creación de estas carpetas si Render no las ha creado
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+GPX_TEMP_DIR.mkdir(parents=True, exist_ok=True)
